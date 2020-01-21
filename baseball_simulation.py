39eb5d4c7b53126eb5a91a15914c29deb3f407b9
Jimmy Simmons <jimmy.simmons@cfacorp.com>
2020-01-20 20:11:02 -0500

    few more updates and move back to monte carlo as the multi-hyp as is takes too long

--- baseball_simulation.py
+++ baseball_simulation.py
@@ -102,7 +102,7 @@ class BaseballState():
     This looks like a tree once it is completed where the leaf nodes are the possible final states
 '''
 
-def run_player_scenario(all_players, order):
+def run_player_scenario_mh(all_players, order):
     players = [all_players[i] for i in order]
 
     runs = [0.0 for i in range(0,MAX_INNINGS*MAX_RUNS_PER_INNING + 1)]
@@ -138,7 +138,131 @@ def run_player_scenario(all_players, order):
         w_sum = w_sum + val*idx
     #return w_sum
     return MAX_INNINGS*MAX_RUNS_PER_INNING - w_sum
+
+'''
+    Runs a a monte-carlo using the players to figure out statistical return
+'''
+
+def run_player_scenario_mc_standalone(all_players, order):
+    players = [all_players[i] for i in order]
+
+    df = pd.DataFrame(data = [], columns = ['Inning', 'Runs', 'Outs', 'Inning Runs'])
+    OUTS = 3
+    MAX_RUNS_PER_INNING = 5
+    MAX_INNINGS = 4
+    MAX_MC_RUNS = 100
     
+    for sim_runs in range(0,MAX_MC_RUNS): 
+        runs = 0 
+        inning = 1 
+        current_batter = 0 
+        bases = [0,0,0] #b1, b2, b3 
+        outs = 0 
+        start_runs = 0
+
+        while inning <= MAX_INNINGS:
+            batting = True
+            result = players[current_batter % len(players)].get_hit()
+            current_batter = current_batter + 1
+            if (result == 'pbo') or (result == 'pso'):
+                outs = outs + 1
+                if (outs == 3):
+                    #max outs advance inning
+                    df.loc[sim_runs*MAX_INNINGS + inning] = (inning, runs, outs, runs - start_runs)
+                    inning = inning + 1
+                    bases = [0,0,0]
+                    outs = 0
+                    start_runs = runs
+                elif (result == 'pbo'):
+                    if (bases[2]):
+                        runs = runs + 1
+                        bases[2] = bases[1]
+                        bases[1] = bases[0]
+                        bases[0] = 0
+                        #max runs advance inning
+                        if ((runs - start_runs)%MAX_RUNS_PER_INNING == 0):
+                            df.loc[sim_runs*MAX_INNINGS + inning] = (inning, runs, outs, runs - start_runs)
+                            inning = inning + 1
+                            bases = [0,0,0]
+                            outs = 0
+                            start_runs = runs
+                    else:
+                        bases[2] = bases[1]
+                        bases[1] = bases[0]
+                        bases[0] = 0
+            elif (result == 'p1b'):
+                if (bases[2]):
+                    runs = runs + 1
+                    bases[2] = bases[1]
+                    bases[1] = bases[0]
+                    bases[0] = 1
+                    #max runs advance inning
+                    if ((runs - start_runs)%MAX_RUNS_PER_INNING == 0):
+                        df.loc[sim_runs*MAX_INNINGS + inning] = (inning, runs, outs, runs - start_runs)
+                        inning = inning + 1
+                        bases = [0,0,0]
+                        outs = 0
+                        start_runs = runs
+                else:
+                    bases[2] = bases[1]
+                    bases[1] = bases[0]
+                    bases[0] = 1
+            elif (result == 'p2b'):
+                if (bases[2] or bases[1]):
+                    runs = min(runs + bases[2] + bases[1],start_runs + MAX_RUNS_PER_INNING)
+                    bases[2] = bases[0]
+                    bases[1] = 1
+                    bases[0] = 0
+                    #max runs advance inning
+                    if ((runs - start_runs)%MAX_RUNS_PER_INNING == 0):
+                        df.loc[sim_runs*MAX_INNINGS + inning] = (inning, runs, outs, runs - start_runs)
+                        inning = inning + 1
+                        bases = [0,0,0]
+                        outs = 0
+                        start_runs = runs
+                else:
+                    bases[2] = bases[0]
+                    bases[1] = 1
+                    bases[0] = 0
+            elif (result == 'p3b'):
+                if (bases[2] or bases[1] or bases[0]):
+                    runs = min(runs + bases[2] + bases[1] + bases[0],start_runs + MAX_RUNS_PER_INNING)
+                    bases[2] = 1
+                    bases[1] = 0
+                    bases[0] = 0
+                    #max runs advance inning
+                    if ((runs - start_runs)%MAX_RUNS_PER_INNING == 0):
+                        df.loc[sim_runs*MAX_INNINGS + inning] = (inning, runs, outs, runs - start_runs)
+                        inning = inning + 1
+                        bases = [0,0,0]
+                        outs = 0
+                        start_runs = runs
+                else:
+                    bases[2] = 1
+                    bases[1] = 0
+                    bases[0] = 0
+            elif (result == 'phr'):
+                runs = min(runs + bases[2] + bases[1] + bases[0] + 1,start_runs + MAX_RUNS_PER_INNING)
+                bases[2] = 0
+                bases[1] = 0
+                bases[0] = 0
+                #max runs advance inning
+                if ((runs - start_runs)%MAX_RUNS_PER_INNING == 0):
+                    df.loc[sim_runs*MAX_INNINGS + inning] = (inning, runs, outs, runs - start_runs)
+                    inning = inning + 1
+                    bases = [0,0,0]
+                    outs = 0
+                    start_runs = runs
+    return df
+    
+
+#Standalone runner to get a min
+def run_player_scenario_mc(all_players, order):
+    df = run_player_scenario_mc_standalone(all_players,order)
+    average_total_runs = df[df.Inning == MAX_INNINGS]['Runs'].sum()/df[df.Inning == MAX_INNINGS]['Runs'].count()
+    #return max runs - average_total since it is a cost not a score
+    return MAX_INNINGS*MAX_RUNS_PER_INNING - average_total_runs 
+
 def test():
     players =[Player([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),          
           Player([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
@@ -150,8 +274,9 @@ def test():
           Player([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
           Player([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
           Player([1.0, 0.0, 0.0, 0.0, 0.0, 0.0])]
-    w_sum = run_player_scenario(players, [0,1,2,3,4,5,6,7,8,9])
-    print(w_sum)
+    w_sum_mh = run_player_scenario_mh(players, [0,1,2,3,4,5,6,7,8,9])
+    w_sum_mc = run_player_scenario_mc(players, [0,1,2,3,4,5,6,7,8,9])
+    print(w_sum_mh,w_sum_mc)
     
     players =[Player([0.5, 0.0, 0.0, 0.0, 0.0, 0.5]),          
           Player([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
@@ -163,9 +288,11 @@ def test():
           Player([0.5, 0.0, 0.0, 0.0, 0.0, 0.5]),
           Player([1.0, 0.0, 0.0, 0.0, 0.0, 0.0]),
           Player([1.0, 0.0, 0.0, 0.0, 0.0, 0.0])]
-    w_sum = run_player_scenario(players, [0,1,2,3,4,5,6,7,8,9])
-    print(w_sum)
+    w_sum_mh = run_player_scenario_mh(players, [0,1,2,3,4,5,6,7,8,9])
+    w_sum_mc = run_player_scenario_mc(players, [0,1,2,3,4,5,6,7,8,9])
+    print(w_sum_mh,w_sum_mc)
     
-    w_sum = run_player_scenario(players, [0,1,2,3,4,5,6,7,8,9])
-    print(w_sum)
+    w_sum_mh = run_player_scenario_mh(players, [0,1,2,3,4,5,6,7,8,9])
+    w_sum_mc = run_player_scenario_mc(players, [0,1,2,3,4,5,6,7,8,9])
+    print(w_sum_mh,w_sum_mc)
     
